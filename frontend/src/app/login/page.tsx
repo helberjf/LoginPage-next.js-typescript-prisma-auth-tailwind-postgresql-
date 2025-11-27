ae5cdb29c22177c6f469505bd41abfa5fc2208a9
helberjf <helberjf@gmail.com>
2025-11-27 02:43:07 -0300

    changing tailwind

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -36,8 +36,8 @@ export default function LoginPage() {
     <div className="min-h-screen flex items-center justify-center bg-background dark:bg-dark-background px-4">
       <div className="w-full max-w-md bg-white dark:bg-dark-background text-black dark:text-dark-foreground p-10 rounded-3xl shadow-2xl">
         {/* Título */}
-        <h1 className="text-4xl font-extrabold mb-2 text-center">Bem-vindo</h1>
-        <p className="text-gray-600 dark:text-gray-300 mb-8 text-center">
+        <h1 className="text-4xl font-extrabold mb-2 text-center text-gray-700">Bem-vindo</h1>
+        <p className="text-gray-600 dark:text-gray-700 mb-8 text-center">
           Use seu email e senha para entrar
         </p>
 
@@ -61,12 +61,12 @@ export default function LoginPage() {
             {...register("password")}
           />
 
-          <div className="flex items-center justify-between text-sm text-gray-700 dark:text-gray-300">
+          <div className="flex items-center justify-between text-sm text-gray-700 dark:text-gray-700">
             <label className="inline-flex items-center">
               <input
                 {...register("remember")}
                 type="checkbox"
-                className="h-4 w-4 rounded border-gray-300 dark:border-gray-500"
+                className="h-4 w-4 rounded border-gray-300 dark:border-gray-700"
               />
               <span className="ml-2">Lembrar-me</span>
             </label>
@@ -86,14 +86,14 @@ export default function LoginPage() {
           <button
             type="submit"
             disabled={isSubmitting}
-            className="w-full py-3 rounded-xl font-semibold text-black bg-primary hover:bg-primary-dark dark:bg-primary-dark dark:hover:bg-primary transition-colors duration-200 disabled:opacity-50"
+            className="w-full py-3 rounded-xl border-2 border-solid border-blue-950 hover:opacity-60 bg font-semibold text-black bg-primary hover:bg-primary-dark cursor-pointer"
           >
             {isSubmitting ? "Entrando..." : "Entrar"}
           </button>
         </form>
 
         {/* Link secundário */}
-        <div className="mt-8 text-center text-sm text-gray-600 dark:text-gray-300">
+        <div className="mt-8 text-center text-sm text-gray-600 dark:text-gray-500">
           <p>
             Não tem conta?{" "}
             <a
