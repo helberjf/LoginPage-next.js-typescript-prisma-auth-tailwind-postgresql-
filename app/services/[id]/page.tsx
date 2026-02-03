11f13f8264c419ef41a9c99bb99942faac21bf83
Helber Soares <helberjf@gmail.com>
2026-02-03 05:50:47 -0300

    fix next/link

--- app/services/[id]/page.tsx
+++ app/services/[id]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import Link from "next/link";
 import Image from "next/image";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
